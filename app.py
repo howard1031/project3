d8f6f3098bd6871d5981f8b8d3006d9a77bcac75
howard1031 <a82951386@gmail.com>
2022-11-14 16:23:28 +0800

    modify

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-print("Hello! Python")
-print("Hello! Python2")
+import private
+import datasource
+
+
+def main():
+    api_key = private.secret.open_weather_key
+    city = datasource.get_forcase_data("Taipei", api_key)
+    print(city)
+
+
+if __name__ == "__main__":
+    main()
